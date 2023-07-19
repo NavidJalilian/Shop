6d20745ba577d564d55c5ca9ff1c83837744917f
Navid Jalilian <n.jalilian@aranumadc.local>
2023-07-19 22:51:37 +0330

    add first page

--- shop/tailwind.config.js
+++ shop/tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        // Add custom colors here
+        purple: "#8A33FD",
+        darkGray: "#3C4242",
+        gray: "#807D7E",
+        white: "White",
+        lightGray: "#BEBCBD",
+        backgroundGray: "#EDEEF2",
+        offWhite: "#F6F6F6",
       },
     },
   },
+  variants: {},
   plugins: [],
-}
+  extend: {
+    backgroundImage: {
+      "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      "gradient-conic":
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    },
+  },
+  plugins: [],
+};
